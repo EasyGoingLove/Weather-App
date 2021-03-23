b012800f09d1487e9bd7c62a24be6b5186fb8844
Tsvetomir <tsvetomirstoev8@gmail.com>
2021-03-23 16:04:55 +0200

    New templates, update on partials , change to souce app.js and styles.css

--- src/app.js
+++ src/app.js
@@ -1,15 +1,49 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+
+// Define paths for Express config
 const publicDirPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
 
+//Setup static dir to serve
 app.use(express.static(publicDirPath))
 
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name:  'JOnny sins'
+    })
+})
 
-app.get('/weather' , (req,res) =>{
-    res.send('lllllllllll')
+app.get('/about', (req,res) =>{
+    res.render('about', {
+        title: 'About the weather app',
+        name:  'JOnny sins'
+    })
+})
+
+app.get('/about/*', (req,res) => {
+    res.render('errorPage', {
+        title: 'About 404',
+        message: 'About article not found'
+    })
+})
+
+app.get('*',(req,res) => {
+    res.render('errorPage', {
+        title: '404 Not Found',
+        message: 'Error 404 Page not found'
+        
+    })
 })
 
 app.listen(3000, () =>{
